9885f66b5d92fd7f95a538c5425ba741c86f4b3b
GULFIER LEONEL LOPEZ VAZQUEZ <gllopezv@ad.elektra.com.mx>
2021-02-03 17:39:55 -0600

    se elimina datos

--- poc-persistence/src/main/java/mx/com/prosa/poc/persistence/TblServidoresRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.com.prosa.poc.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mx.com.prosa.poc.model.TblServidores;
-
-public interface TblServidoresRepository extends JpaRepository<TblServidores, Long> {
-	
-}
